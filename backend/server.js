e7371e3a4d6605104d988c747b9a5f4331b87e6c
Z-M-STA <marios_30@windowslive.com>
2025-03-21 11:24:16 +0200

    before trying produciton

--- backend/server.js
+++ backend/server.js
@@ -33,12 +33,26 @@ app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req,res) => res.send({clientId: process.env.PAYPAL_CLIENT_ID}));
 
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
+
 const __dirname = path.resolve(); 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
-
-
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

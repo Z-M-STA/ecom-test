6d4afe1f2329b296f272fce0417affd5fe4f6489
Z-M-STA <marios_30@windowslive.com>
2025-03-21 11:47:04 +0200

    before trying produciton v2

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({extended: true}));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -48,9 +44,6 @@ if (process.env.NODE_ENV === 'production') {
       res.send('API is running....');
     });
   }
-
-const __dirname = path.resolve(); 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
